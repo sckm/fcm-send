ad98dec490b9f7804a05ea77539a8b26f02f8f0b
scache <scal.che@gmail.com>
2017-07-20 01:13:28 +0900

    get token, server key and data payload from arguments

--- main.go
+++ main.go
@@ -1,15 +1,49 @@
 package main
 
-import "fmt"
-import "os"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	data := "{\"Hello\":1, \"obj\":[{\"a\":1}]}"
+	var (
+		to        string
+		serverKey string
+		data      string
+	)
 
-	// TODO get server key from arg
-	serverKey := "server key"
-	// TODO get registration id from arg
-	to := "registration id"
+	flag.StringVar(&to, "t", "", "Registration token, Topic name or Device group name")
+	flag.StringVar(&serverKey, "s", "", "server key")
+	flag.StringVar(&data, "d", "",
+		`data payload. ex. {"message":"Hello World!"}`)
+
+	if len(os.Args) == 1 {
+		fmt.Println("require arguments\n")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	flag.Parse()
+
+	if to == "" {
+		fmt.Println("require Registration token, Topic name or Device group name.")
+		fmt.Println("use -t argument")
+		os.Exit(1)
+	}
+
+	if serverKey == "" {
+		fmt.Println("require server key")
+		fmt.Println("use -s argument")
+		os.Exit(1)
+	}
+
+	if data == "" {
+		fmt.Println("require data payload")
+		fmt.Println("use -d argument")
+		os.Exit(1)
+	}
+
 	err := sendData(serverKey, data, to)
 	if err != nil {
 		fmt.Println(err)
